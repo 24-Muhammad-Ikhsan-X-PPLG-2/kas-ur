287cc0767def013e080e218fd2982621c001ab5b
24-Muhammad-Ikhsan-X-PPLG-2 <ikhsanm181209@gmail.com>
2026-09-26 12:56:50 +0700

    feat(analytics): menambahkan fitur analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
         {children}
         <ToastContainer />
+        <Analytics />
       </body>
     </html>
   );
